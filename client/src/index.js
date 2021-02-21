9702b3fb1cf08ef57b51bcc9032a4ffa8afbac1d
Mark Thrasher <drmarkthrasher@gmail.com>
2021-02-21 12:21:21 -0600

    unsure of these changes.  It seems to be Docker stuff

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,9 @@ import { ApolloProvider } from '@apollo/client';
 import { GlobalProvider } from './context/GlobalState';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000',
+  // in line below, put in ip address of machine hosting server
+  // if local, can use localhost
+  uri: 'http://192.168.86.37:5000',
   cache: new InMemoryCache({ addTypename: true }),
   headers: {
     Authorization: `${localStorage.getItem('token')}`,
